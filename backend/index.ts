ec8ac100fc377467aa409fe0ee52d46e6a34ed13
Kevin <kramzyvine@icloud.com>
2026-01-17 22:06:02 +0300

    Create database models for Messages, Chats and Users

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello via Bun!");
+import app from "./src/app";
+import mongoose from "mongoose";
+
+const PORT = process.env.PORT || 4960;
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/chat_db";
+
+mongoose.connect(MONGO_URI).then(() => {
+  console.log("\n\nConnected to MongoDB Atlas Successfully!");
+
+  app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}\n\nHappy Hacking!`);
+});
+
+}).catch((error) => {
+  console.error("Error connecting to MongoDB:", error);
+});
---
+++ backend/src/models/Chat.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema, type Document, model } from "mongoose";
+
+export interface IChat extends Document {
+    participants: mongoose.Types.ObjectId[];
+    lastMessage?: mongoose.Types.ObjectId;
+    lastMessageAt?: Date; 
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const ChatSchema = new Schema<IChat>({
+    participants: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
+    lastMessage: { type: Schema.Types.ObjectId, ref: "Message", default: null },
+    lastMessageAt: { type: Date, default: null },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+}, {
+    timestamps: true,
+});
+
+export const Chat = model("Chat", ChatSchema);
