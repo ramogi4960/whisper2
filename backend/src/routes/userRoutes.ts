fb4861c358841af485d09cb16aa5fd3cde12ab5d
Kevin <kramzyvine@icloud.com>
2026-01-30 23:29:00 +0300

    Add user retrieval functionality with protected route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,6 +1,16 @@
 import { Router } from "express";
+import { protectRoute } from "../middleware/auth";
+import { getUsers } from "../controllers/userController";
+
+/*
+    User Routes will be defined here
+    get all users
+    post and create new user
+*/
 
 
 const router = Router();
+
+router.get("/", protectRoute, getUsers);
 
 export default router;
